65f302eeb9450d0af3396c2f1b26d228e61040d5
ShadowSKD <76768791+ShadowSKD@users.noreply.github.com>
2026-05-20 15:23:31 +0530

    GOAT Login

--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AuthProvider as CognitoProvider } from "react-oidc-context";
+import { Amplify } from 'aws-amplify';
 import { AuthProvider as LocalAuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 
-const cognitoAuthConfig = {
-  authority:
-    import.meta.env.VITE_COGNITO_AUTHORITY,
+const authorityParts = import.meta.env.VITE_COGNITO_AUTHORITY?.split('/') || [];
+const userPoolId = authorityParts[authorityParts.length - 1];
 
-  client_id:
-    import.meta.env.VITE_COGNITO_CLIENT_ID,
-
-  redirect_uri:
-    import.meta.env.VITE_COGNITO_REDIRECT_URI,
-
-  response_type: "code",
-
-  scope: "openid email phone",
-};
+if (userPoolId) {
+  Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId,
+        userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+        signUpVerificationMethod: 'code',
+        loginWith: {
+          email: true,
+          phone: false,
+          username: true
+        }
+      }
+    }
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CognitoProvider {...cognitoAuthConfig}>
-      <LocalAuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </LocalAuthProvider>
-    </CognitoProvider>
+    <LocalAuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </LocalAuthProvider>
   </React.StrictMode>,
 )
